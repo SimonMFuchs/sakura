c59519fb4e4f500ea081abf17b090821b69bf156
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-05-19 00:16:25 +0200

    11 Navbar ng generate

--- src/app/landingPage/landingPage.component.ts
+++ src/app/landingPage/landingPage.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
     selector: 'app-landingPage',
     standalone: true,
-    imports: [],
+    imports: [NavbarComponent],
     template: `
     <section>
-        <h1>SAKURA RAMEN</h1>
-        <h2>BEST RAMEN IN TOWN</h2>
+        <app-navbar></app-navbar>
+        <h1 class="fontRaleway">SAKURA RAMEN</h1>
+        <h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
     </section>
     `,
     styleUrls: ['./landingPage.component.scss']    
