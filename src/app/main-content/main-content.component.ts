ac986965cf664a98e45d8aeebff598fc38b53613
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-05-19 19:15:02 +0200

    15 Ready for testing

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landingPage.component';
 import { OurMenuComponent } from './our-menu/our-menu.component';
+import { HowToOrderComponent } from './how-to-order/how-to-order.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [LandingPageComponent, OurMenuComponent],
+  imports: [LandingPageComponent, OurMenuComponent, HowToOrderComponent, FooterComponent],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+  styleUrl: './main-content.component.scss',
 })
-export class MainContentComponent {
-
-}
+export class MainContentComponent {}
