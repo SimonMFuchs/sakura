b9733177fe90a6e978e7caa12ddd4fbbb411c960
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-05-19 21:27:30 +0200

    15 Add styling for how-to-order and our-menu component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from "./main-content/main-content.component";
 import { ImprintComponent } from './imprint/imprint.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainContentComponent, ImprintComponent],
+  imports: [RouterOutlet, MainContentComponent, ImprintComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
